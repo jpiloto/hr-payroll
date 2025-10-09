592d592c26925d4ef675d1a8c55b149f8867ebc7
pilot <pilot9280ster@gmail.com>
2025-10-08 22:35:08 -0400

    chore: add messaging dependencies, exclude stream/function auto-config in tests, include test application.yml

--- src/test/java/com/hrapp/hr_payroll/HrPayrollApplicationTests.java
+++ src/test/java/com/hrapp/hr_payroll/HrPayrollApplicationTests.java
@@ -3,7 +3,11 @@ package com.hrapp.hr_payroll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+		"spring.cloud.stream.default.producer.enabled=false",
+		"spring.cloud.stream.default.consumer.enabled=false",
+		"spring.cloud.function.scan.enabled=false"
+})
 class HrPayrollApplicationTests {
 
 	@Test

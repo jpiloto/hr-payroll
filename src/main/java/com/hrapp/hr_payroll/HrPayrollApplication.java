982de42dd8aa1a69ea918c12a1dbe32ba513f203
pilot <pilot9280ster@gmail.com>
2025-10-24 17:29:20 -0400

    chore: log for troubleshooting

--- src/main/java/com/hrapp/hr_payroll/HrPayrollApplication.java
+++ src/main/java/com/hrapp/hr_payroll/HrPayrollApplication.java
@@ -1,14 +1,17 @@
 package com.hrapp.hr_payroll;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.kafka.annotation.EnableKafka;
 
+@Slf4j
 @SpringBootApplication
 @EnableKafka
 public class HrPayrollApplication {
 
 	public static void main(String[] args) {
+		log.info("🔥 Startup checkpoint reached");
 		SpringApplication.run(HrPayrollApplication.class, args);
 	}
 
